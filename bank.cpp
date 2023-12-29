bee525396fd75ed9e5ad49f2641a198b1791bc10
Vu <longvunguyen2003@gmail.com>
2023-12-29 10:07:06 +0700

    BASIC

--- bank.cpp
+++ bank.cpp
@@ -2,10 +2,11 @@
 #include <termios.h>
 #include <unistd.h>
 #include <fstream>
+
 using namespace std;
 
-map<string, tuple<string, int>> users; // user: (password, amount)
-map<string, string> admins; // admin: password
+unordered_map<string, tuple<string, int, string>> users; // user: (password, amount, name)
+unordered_map<string, string> admins; // admin: password
 int author = 0; // 0-not logined, 1-user, 2-admin
 string currUser;
 string getDate(){
@@ -50,8 +51,8 @@ void initiate(){
     ifstream accountFile("./user/account.txt");
     string userInfo;
     while (getline(accountFile, userInfo)){
-        vector<string> info = split(userInfo, ' ');
-        users[info[0]] = make_tuple(info[1], stoi(info[2]));
+        vector<string> info = split(userInfo, '+');
+        users[info[0]] = make_tuple(info[1], stoi(info[2]), info[3]);
     }
     accountFile.close();
 
@@ -63,13 +64,31 @@ void initiate(){
     }
     adminFile.close();
 }
+void save(){
+    ofstream userFile("./user/account.txt");
+    for (auto i : users){
+        string account = i.first;
+        string password = get<0>(i.second);
+        string name = get<2>(i.second);
+        int amount = get<1>(i.second);
+        userFile << account + "+" + password + "+" + to_string(amount) + "+" + name + '\n'; 
+    } 
+    userFile.close();
+
+    ofstream adminFile("./user/admin.txt");
+    for (auto i : admins){
+        string adminName = i.first;
+        string password = i.second;
+        adminFile << adminName + " " + password + '\n';
+    }
+}
 int isNumber(string number){
     return number.find_first_not_of("0123456789") == std::string::npos;
 }
 void createUserAccount(){
     string username;
     string password;
-
+    string name;
     // create username
     do{
         cout << "Enter your username: ";  
@@ -81,7 +100,11 @@ void createUserAccount(){
             break;
         }
     } while(1);
-
+    
+    cout << "Enter your name: ";
+    cin >> ws;  
+    getline(cin, name);
+    
     // create password
     termios oldt;
     tcgetattr(STDIN_FILENO, &oldt);
@@ -122,10 +145,10 @@ void createUserAccount(){
     // store account
     ofstream accountFile;
     accountFile.open("./user/account.txt", ios::app);
-    accountFile << (username + " " + password + " " + '0' + '\n');
+    accountFile << (username + "+" + password + "+" + "0" + "+" + name + '\n');
     accountFile.close();
 
-    users[username] = make_tuple(password, 0);
+    users[username] = make_tuple(password, 0, name);
 }
 void userLogin(){
     string username;
@@ -256,6 +279,43 @@ void createAdminAccount(){
 
     admins[username] = password;
 }
+void viewTranmissionHistory(){
+    ifstream tranmissionFile("./user/transmission.txt");
+    string line;
+    while (getline(tranmissionFile, line)){
+        vector<string> arr = split(line, ' ');
+        string fromUser = arr[0];
+        string toUser = arr[1];
+        if (currUser == fromUser){
+            cout << "You transfered " + arr[2] + " to " + get<2>(users[arr[1]]) + " on " + split(arr[3], '/')[0] + " at " + split(arr[3], '/')[1] << '\n'; 
+        }
+        else if (currUser == toUser){
+            cout << get<2>(users[fromUser]) + " transfered " + arr[2] + " to you " + " on " + split(arr[3], '/')[0] + " at " + split(arr[3], '/')[1] << '\n'; 
+        }
+    }
+}
+void viewWithdrawalHistory(){
+    ifstream withdrawnFile("./user/withdrawn.txt");
+    string line;
+    while (getline(withdrawnFile, line)){
+        vector<string> arr = split(line, ' ');
+        if (arr[0] != currUser){
+            continue;
+        }
+        cout << "You withdrawned " + arr[1] + " on " + split(arr[2], '/')[0] + " at " + split(arr[2], '/')[1] << '\n';
+    }
+}
+void viewDepositHistory(){
+    ifstream depositFile("./user/deposit.txt");
+    string line;
+    while (getline(depositFile, line)){
+        vector<string> arr = split(line, ' ');
+        if (arr[0] != currUser){
+            continue;
+        }
+        cout << "You deposited " + arr[2] + " on " + split(arr[3], '/')[0] + " at " + split(arr[3], '/')[1] << '\n';
+    }
+}
 void transferMoney(string username, int amount){
     get<1>(users[currUser]) -= amount;
     get<1>(users[username]) += amount;
@@ -266,25 +326,51 @@ void transferMoney(string username, int amount){
     transmissionFile.close();
     return;
 }
+void withdrawnMoney(int amount){
+    get<1>(users[currUser]) -= amount;
+    string date = getDate();
+    ofstream transmissionFile;
+    transmissionFile.open("./user/withdrawn.txt", ios::app);
+    transmissionFile << currUser + " " + to_string(amount) + " " + date + '\n';
+    transmissionFile.close();
+    return;
+}
 void checkamount(string user){
     cout << "Your balance is: " << get<1>(users[user]);
 }
 
 void displayUserMenu(){
     system("clear");
-    int exist = 0;
+    int exit = 0;
     int choice;
     do{
         cout << "--------------------" << '\n';
+        cout << "Hello " << get<2>(users[currUser]) << '\n';
         cout << "USER'S MENU" << '\n';
         cout << "--------------------" << '\n';
         cout << "Please choose a number" << '\n';
         cout << "1. Check Balance" << '\n';
         cout << "2. Transfer Money" << '\n';
-        cout << "3. Exist" << '\n';
-        cout << "--------------------" << '\n';
-        cout << "Your choice: ";
-        cin >> choice;
+        cout << "3. Withdrawn Money" << '\n';
+        cout << "4. View Transmission History" << '\n';
+        cout << "5. View Withdrawal History" << '\n';
+        cout << "6. View Deposit History" << '\n';
+        cout << "7. Exit" << '\n';
+        string input;
+        do{
+            cout << "--------------------" << '\n';
+            cout << "Your choice: ";
+            cin >> input;
+            if (!isNumber(input)){
+                cout << "Please Enter A Number" << '\n';
+                continue;
+            }
+            else{
+                choice = stoi(input);
+                break;
+            }  
+        }while(1);
+        
         switch (choice){
             case 1: {
                 system("clear");
@@ -315,8 +401,11 @@ void displayUserMenu(){
 
                 do {
                     string amount;
-                    cout << "Enter The Amount Of Money: ";
+                    cout << "Enter The Amount Of Money (Or enter 'n' to exit): ";
                     cin >> amount;
+                    if (amount == "n"){
+                        break;
+                    }
                     if (!isNumber(amount)){
                         cout << "Please Enter A Number" << '\n';
                     }
@@ -364,7 +453,91 @@ void displayUserMenu(){
                 break;
             }
             case 3: {
-                exist = 1;
+                system("clear");
+                cout << "WITHDRAWN MONEY" << '\n';
+                do {
+                    string amount;
+                    cout << "Enter The Amount Of Money (Or enter 'n' to exit): ";
+                    cin >> amount;
+                    if (amount == "n"){
+                        break;
+                    }
+                    if (!isNumber(amount)){
+                        cout << "Please Enter A Number" << '\n';
+                    }
+                    else if (stoi(amount) > get<1>(users[currUser])){
+                        cout << "You don't have enough money" << '\n';
+                        cout << "Enter 0 to continue" << '\n';
+                        string tmp;
+                        cin >> tmp;
+                        break;
+                    }
+                    else{
+                        // enter password
+                        termios oldt;
+                        tcgetattr(STDIN_FILENO, &oldt);
+                        termios newt = oldt;
+                        newt.c_lflag &= ~ECHO;
+                        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+                        string password;
+                        do{
+                            password = "";
+                            cout << "Enter your password: ";
+                            cin >> ws;
+                            while (1){
+                                char c = cin.get();
+                                if (c == '\n'){
+                                    break;
+                                }
+                            password += c;
+                            }
+                            cout << '\n';
+                            if (password == get<0>(users[currUser])){
+                                break;
+                            }
+                        }while (1);
+                        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+                        // transfer money
+                        withdrawnMoney(stoi(amount));
+                        cout << "SUCCESSFULLY WITHDRAWN MONEY" << '\n';
+                        cout << "Enter 0 To Return" << '\n';
+                        string in;
+                        cin >> in;
+                        break;
+                    }
+                }while (1);
+                break;
+            }
+            case 4: {
+                system("clear");
+                cout << "TRANSMISSION HISTORY" << '\n';
+                viewTranmissionHistory();
+                cout << '\n' << "Enter 0 To Return" << '\n';
+                string in;
+                cin >> in;
+                break;
+            }
+            case 5:{
+                system("clear");
+                cout << "WITHDRAWAL HISTORY" << '\n';
+                viewWithdrawalHistory();
+                cout << '\n' << "Enter 0 To Return" << '\n';
+                string in;
+                cin >> in;
+                break;
+            }
+            case 6:{
+                system("clear");
+                cout << "DEPOSIT HISTORY" << '\n';
+                viewDepositHistory();
+                cout << '\n' << "Enter 0 To Return" << '\n';
+                string in;
+                cin >> in;
+                break;
+            }
+            case 7: {
+                exit = 1;
+                save();
                 break;
             }
             default: {
@@ -376,11 +549,11 @@ void displayUserMenu(){
             }
         }
         system("clear");
-    } while(!exist);
+    } while(!exit);
 }
 void displayAdminMenu(){
     system("clear");
-    int exist = 0;
+    int exit = 0;
     int choice;
     do{
         cout << "--------------------" << '\n';
@@ -389,10 +562,21 @@ void displayAdminMenu(){
         cout << "Please choose a number" << '\n';
         cout << "1. Deposit Money" << '\n';
         cout << "2. Check Balance" << '\n';
-        cout << "3. Exist" << '\n';
-        cout << "--------------------" << '\n';
-        cout << "Your choice: ";
-        cin >> choice;
+        cout << "3. Exit" << '\n';
+         string input;
+        do{
+            cout << "--------------------" << '\n';
+            cout << "Your choice: ";
+            cin >> input;
+            if (!isNumber(input)){
+                cout << "Please Enter A Number" << '\n';
+                continue;
+            }
+            else{
+                choice = stoi(input);
+                break;
+            }  
+        }while(1);
         switch (choice){
             case 1: {
                 system("clear");
@@ -448,7 +632,7 @@ void displayAdminMenu(){
                         get<1>(users[userAccount]) += stoi(amount);
                         ofstream depositFile;
                         depositFile.open("./user/deposit.txt", ios::app);
-                        depositFile << currUser + " " + userAccount + " " + amount + ' ' + getDate();
+                        depositFile << userAccount + " " + currUser + " " + amount + ' ' + getDate() + '\n';
                         cout << "SUCCESSFULLY DEPOSIT MONEY" << '\n';
                         cout << "Enter 0 To Return" << '\n';
                         string in;
@@ -468,7 +652,7 @@ void displayAdminMenu(){
                     cin >> userAccount;
                     cout << '\n';
                     if (users.find(userAccount) != users.end()){
-                        cout << userAccount << "'s balance: " << get<1>(users[userAccount]);
+                        cout << userAccount << "'s balance: " << get<1>(users[userAccount]) << '\n';
                         break;
                     }
                     else {
@@ -481,7 +665,8 @@ void displayAdminMenu(){
                 break;
             }
             case 3: {
-                exist = 1;
+                exit = 1;
+                save();
                 break;
             }
             default: {
@@ -493,10 +678,10 @@ void displayAdminMenu(){
             }
         }
         system("clear");
-    } while(!exist);
+    } while(!exit);
 }
 void displayMenu(){
-    int exist = 0;
+    int exit = 0;
     int choice;
     do{
         cout << "--------------------" << '\n';
@@ -507,10 +692,21 @@ void displayMenu(){
         cout << "2. Login as an admin" << '\n';
         cout << "3. Create an user account" << '\n';
         cout << "4. Create an admin account" << '\n';
-        cout << "5. Exist" << '\n';
-        cout << "--------------------" << '\n';
-        cout << "Your choice: ";
-        cin >> choice;
+        cout << "5. Exit" << '\n';
+        string input;
+        do{
+            cout << "--------------------" << '\n';
+            cout << "Your choice: ";
+            cin >> input;
+            if (!isNumber(input)){
+                cout << "Please Enter A Number" << '\n';
+                continue;
+            }
+            else{
+                choice = stoi(input);
+                break;
+            }  
+        }while(1);
         switch (choice){
             case 1: {
                 system("clear");
@@ -547,7 +743,7 @@ void displayMenu(){
             case 4:{
                 system("clear");
                 cout << "--------------------" << '\n';
-                cout << "Create an user account" << '\n';
+                cout << "Create an admin account" << '\n';
                 createAdminAccount();
                 cout << '\n' << "Enter 0 To Continue" << '\n';
                 string in;
@@ -555,7 +751,7 @@ void displayMenu(){
                 break;
             }
             case 5: {
-                exist = 1;
+                exit = 1;
                 break;
             }
             default: {
@@ -567,28 +763,10 @@ void displayMenu(){
             }
         }
         system("clear");
-    } while(!exist);
-}
-void exist(){
-    ofstream userFile("./user/account.txt");
-    for (auto i : users){
-        string account = i.first;
-        string password = get<0>(i.second);
-        int amount = get<1>(i.second);
-        userFile << account + " " + password + " " + to_string(amount) + '\n'; 
-    } 
-    userFile.close();
-
-    ofstream adminFile("./user/admin.txt");
-    for (auto i : admins){
-        string adminName = i.first;
-        string password = i.second;
-        adminFile << adminName + " " + password + '\n';
-    }
+    } while(!exit);
 }
 int main(){
     system("clear");
     initiate();
     displayMenu();
-    exist();
 }
